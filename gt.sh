19e228329d6a4561f28f0479be92ac71db05ce30
PC Liao <peicheng5@gmail.com>
2022-10-06 23:01:08 +0800

    init

---
+++ gt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git checkout --orphan temp $1
+git commit -m "init"
+git rebase --onto temp $1 main
+git branch -D temp
+# The following 2 commands are optional - they keep your git repo in good shape.
+git prune --progress # delete all the objects w/o references
+git gc --aggressive # aggressively collect garbage; may take a lot of time on large repos
